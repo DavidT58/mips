9a227d3f2105ea65e258d87fe4f1bf944ffe5f0b
David Cejtlin <cd180700d@student.etf.bg.ac.rs>
2022-08-25 01:10:09 +0200

    add 7

---
+++ ispit/cubemx_7/code/Core/Inc/example.h
@@ -0,0 +1,13 @@
+/*
+ * example.h
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: Marko Micovic
+ */
+
+#ifndef CORE_INC_EXAMPLE_H_
+#define CORE_INC_EXAMPLE_H_
+
+extern void exampleInit();
+
+#endif /* CORE_INC_EXAMPLE_H_ */
---
+++ ispit/cubemx_7/code/Core/Src/example.c
@@ -0,0 +1,64 @@
+/*
+ * example.c
+ *
+ *  Created on: Dec 28, 2020
+ *      Author: Marko Micovic
+ */
+
+#include "FreeRTOS.h"
+#include "task.h"
+#include "timers.h"
+#include "queue.h"
+
+#include "example.h"
+#include "gpio.h"
+
+QueueHandle_t queueHandle;
+
+TimerHandle_t timer1;
+TimerHandle_t timer2;
+
+void callbackTimer(TimerHandle_t timerHandle);
+void taskToggler(void *parameters);
+
+void exampleInit()
+{
+	queueHandle = xQueueCreate(10, sizeof(uint32_t));
+	xTaskCreate(taskToggler, "taskToggler", 128, NULL, 2, NULL);
+
+	timer1 = xTimerCreate("seconds1", pdMS_TO_TICKS(1000), pdTRUE,
+			(void*) GPIO_PIN_0, callbackTimer);
+	timer2 = xTimerCreate("seconds3", pdMS_TO_TICKS(3000), pdTRUE,
+			(void*) GPIO_PIN_1, callbackTimer);
+
+	xTimerStart(timer1, portMAX_DELAY);
+	xTimerStart(timer2, portMAX_DELAY);
+}
+
+void callbackTimer(TimerHandle_t timerHandle)
+{
+	uint32_t GPIO_Pin = (uint32_t) pvTimerGetTimerID(timerHandle);
+	HAL_GPIO_TogglePin(GPIOC, GPIO_Pin);
+}
+
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	if (GPIO_Pin == GPIO_PIN_2 || GPIO_Pin == GPIO_PIN_3)
+	{
+		uint32_t itemToQueue = GPIO_Pin;
+		BaseType_t woken = pdFALSE;
+		xQueueSendToBackFromISR(queueHandle, &itemToQueue, &woken);
+		portYIELD_FROM_ISR(woken);
+	}
+}
+
+void taskToggler(void *parameters)
+{
+	uint32_t bufferForPinIndex;
+	while (1)
+	{
+		xQueueReceive(queueHandle, &bufferForPinIndex, portMAX_DELAY);
+		HAL_GPIO_TogglePin(GPIOC, bufferForPinIndex);
+	}
+}
+
